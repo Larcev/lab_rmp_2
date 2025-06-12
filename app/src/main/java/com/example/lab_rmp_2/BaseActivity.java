01b45749dafead8500fa223de2c178046679b0f9
Larcev <rustamtastemiroff@yandex.ru>
2025-06-12 07:40:31 +0600

    Инициализация MainActivity и BaseActivity для базовой структуры приложения

---
+++ app/src/main/java/com/example/lab_rmp_2/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.lab_rmp_2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
