2ef71f786dc2e13b512e4a5f059bb0071d7c50e4
Larcev <rustamtastemiroff@yandex.ru>
2025-06-12 07:48:25 +0600

    Реализация DetailFragment и SecondActivity для отображения дополнительных экранов

---
+++ app/src/main/java/com/example/lab_rmp_2/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.lab_rmp_2;
+
+import android.os.Bundle;
+
+public class SecondActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
